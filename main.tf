01a6236e7374c59af7443d2a38ef70e19378b398
DanielObirije <obirijedaniel@gmail.com>
2026-07-27 23:16:46 +0100

    complete main.tf

--- main.tf
+++ main.tf
@@ -151,86 +151,23 @@ resource "aws_cloudwatch_log_group" "lambda_logs" {
 
 # # API GATEWAY RESOURCES
 
-# # Rest API
-# resource "aws_api_gateway_rest_api" "file_sharing_api" {
-#   name = "${local.project_name}-api"
-#   description = "File Sharing API for generating presigned URLs"
-
-#   endpoint_configuration {
-#     types = ["REGIONAL"] #investigate this 
-#   }
-
-#   tags = merge(local.common_tags, {
-#     Name = "File Sharing API"
-#     Description = "API Gateway for file sharing service"
-#   })
-# }
-
+# Rest API
 resource "aws_apigatewayv2_api" "file_sharing_api" {
   name          = "${local.project_name}-api"
   protocol_type = "HTTP"
 
   cors_configuration {
     allow_origins = ["*"]
-    allow_methods = ["GET", "POST"]
+    allow_methods = ["GET", "POST","OPTIONS"]
     allow_headers = ["*"]
   }
 
   tags = merge(local.common_tags, {
     Name = "File Sharing API"
+     Description = "HTTP API for file sharing service"
   })
 }
 
-# # API Gateway Resource: /health
-# resource "aws_api_gateway_resource" "health" {
-#   rest_api_id = aws_api_gateway_rest_api.file_sharing_api.id
-#   parent_id = aws_api_gateway_rest_api.file_sharing_api.root_resource_id #investigate this what is rootresouce
-#   path_part = "health"
-# }
-
-# # API Gateway Resource: /upload-url
-# resource "aws_api_gateway_resource" "upload-url" {
-#   rest_api_id = aws_api_gateway_rest_api.file_sharing_api.id
-#   parent_id = aws_api_gateway_rest_api.file_sharing_api.root_resource_id #investigate this what is rootresouce
-#   path_part = "upload-url"
-# }
-
-# # API Gateway Resource: /download-url
-# resource "aws_api_gateway_resource" "download-url" {
-#   rest_api_id = aws_api_gateway_rest_api.file_sharing_api.id
-#   parent_id = aws_api_gateway_rest_api.file_sharing_api.root_resource_id #investigate this what is rootresouce
-#   path_part = "download-url"
-# }
-
-
-# #API GATEWAY METHODS
-
-# # GET /health
-# resource "aws_api_gateway_method" "health_get" {
-#   rest_api_id = aws_api_gateway_rest_api.file_sharing_api.id
-#   resource_id = aws_api_gateway_resource.health.id
-#   http_method = "GET"
-#   authorization    = "NONE"
-#   api_key_required = false
-# }
-
-# # POST /upload-url
-# resource "aws_api_gateway_method" "upload_url_post" {
-#   rest_api_id = aws_api_gateway_rest_api.file_sharing_api.id
-#   resource_id = aws_api_gateway_resource.upload-url.id
-#   http_method = "POST"
-#   authorization    = "NONE"
-#   api_key_required = false
-# }
-
-# # POST /download-url
-# resource "aws_api_gateway_method" "download_url_post" {
-#   rest_api_id = aws_api_gateway_rest_api.file_sharing_api.id
-#   resource_id = aws_api_gateway_resource.download-url.id
-#   http_method = "POST"
-#   authorization    = "NONE"
-#   api_key_required = false
-# }
 
 # # API GATEWAY INTEGRATIONS
 
@@ -240,36 +177,6 @@ resource "aws_apigatewayv2_integration" "lambda" {
   integration_uri        = aws_lambda_function.presigned_url_api.invoke_arn
   payload_format_version = "2.0"
 }
-
-# # GET /health integration
-# resource "aws_api_gateway_integration" "health_integration" {
-#   rest_api_id = aws_api_gateway_rest_api.file_sharing_api.id
-#   resource_id = aws_api_gateway_resource.health.id
-#   http_method = aws_api_gateway_method.health_get.http_method
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.presigned_url_api.invoke_arn
-# }
-
-# # POST /upload-url integration
-# resource "aws_api_gateway_integration" "upload_url_integration" {
-#   rest_api_id = aws_api_gateway_rest_api.file_sharing_api.id
-#   resource_id = aws_api_gateway_resource.upload-url.id
-#   http_method = aws_api_gateway_method.upload_url_post.http_method
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.presigned_url_api.invoke_arn
-# }
-
-# # POST /download-url integration
-# resource "aws_api_gateway_integration" "download_url_integration" {
-#   rest_api_id             = aws_api_gateway_rest_api.file_sharing_api.id
-#   resource_id             = aws_api_gateway_resource.download-url.id
-#   http_method             = aws_api_gateway_method.upload_url_post.http_method
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = aws_lambda_function.presigned_url_api.invoke_arn
-# }
 
 resource "aws_apigatewayv2_route" "health" {
   api_id    = aws_apigatewayv2_api.file_sharing_api.id
@@ -296,6 +203,64 @@ resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.file_sharing_api.id
   name        = "$default"
   auto_deploy = true
+
+ access_log_settings {
+   destination_arn = aws_cloudwatch_log_group.api_gateway_logs.arn
+   format = jsonencode({
+      requestId               = "$context.requestId"
+      ip                      = "$context.identity.sourceIp"
+      caller                  = "$context.identity.caller"
+      user                    = "$context.identity.user"
+      requestTime             = "$context.requestTime"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      status                  = "$context.status"
+      protocol                = "$context.protocol"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    })
+ }
+ tags = merge(local.common_tags, {
+  Name        = "API Gateway $default Stage"
+  Description = "Default stage with automatic deployment for file sharing API"
+})
+}
+
+
+resource "aws_cloudwatch_log_group" "api_gateway_logs" {
+  name              = "/aws/api-gateway/${aws_api_gateway_rest_api.file_sharing_api.name}"
+  retention_in_days = 7
+  
+  tags = merge(local.common_tags, {
+    Name = "API Gateway Logs"
+    Description = "Access logs for API Gateway"
+  })
+}
+
+# LAMBDA PERMISSIONS FOR API GATEWAY
+resource "aws_lambda_permission" "api_gateway_health" {
+   statement_id  = "AllowAPIGatewayInvoke-Health"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.presigned_url_api.function_name
+   principal     = "apigateway.amazonaws.com"
+   source_arn = "${aws_apigatewayv2_api.file_sharing_api.execution_arn}/*/GET/health"
+}
+
+
+resource "aws_lambda_permission" "api_gateway_upload" {
+   statement_id  = "AllowAPIGatewayInvoke-Upload"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.presigned_url_api.function_name
+   principal     = "apigateway.amazonaws.com"
+   source_arn = "${aws_apigatewayv2_api.file_sharing_api.execution_arn}/*/POST/upload-url"
+}
+
+resource "aws_lambda_permission" "api_gateway_download" {
+   statement_id  = "AllowAPIGatewayInvoke-Download"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.presigned_url_api.function_name
+   principal     = "apigateway.amazonaws.com"
+   source_arn = "${aws_apigatewayv2_api.file_sharing_api.execution_arn}/*/POST/download-url"
 }
 
 
@@ -446,64 +411,6 @@ resource "aws_s3_bucket_lifecycle_configuration" "file_sharing" {
   }
 }
 
-# IAM role for presigned URL generation (for applications/services)
-resource "aws_iam_role" "presigned_url_generator" {
-  name = "${local.bucket_name}presigned-url-generator"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement =[
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = [
-          "lambda.amazonaws.com",
-          "ec2.amazonaws.com"
-        ]
-      }
-    ]
-    AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-  })
-
-  tags = merge(local.common_tags,{
-    Name = "Presigned URL Generator Role"
-    Description = "IAM role for generating presigned URLs"
-  })
-}
-
-# IAM policy for presigned URL generation
-
-resource "aws_iam_role_policy" "presigned_url_generator" {
-  name = "${local.bucket_name}presigned-url-policy"
-  role = aws_iam_role.presigned_url_generator.id
-
-  policy = jsondecode({
-       Version = "2012-10-17"
-       Statement = [
-        {
-          Effect = "Allow"
-          Action = [
-            "s3:GetObject",
-            "s3:PutObject",
-            "s3:DeleteObject",
-            "s3:GetObjectVersion",
-            "s3:PutObjectAcl"
-          ]
-          Resource = [
-            "${aws_s3_bucket.file_sharing.arn}/*"
-          ]
-        },
-        {
-          Effect = "Allow"
-          Action = [
-          "s3:ListBucket",
-          "s3:GetBucketLocation",
-          "s3:GetBucketVersioning"
-          ]
-          Resource = "${aws_s3_bucket.file_sharing.arn}"
-        }
-       ]
-  })
-}
 
 # CloudWatch Log Group for monitoring S3 access (if CloudTrail is enabled)
 
